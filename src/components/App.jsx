99677bec116b7ae02ac3b660879bcccaba0ec318
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-03-10 15:14:22 +0200

    task 2 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,33 @@
-import { Input } from './input/input';
-export const App = () => {
-  // state = {
-  //   contacts: [],
-  //   name: '',
-  // };
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Input />
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './contact-form/ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <div>
+          <h1>Phonebook</h1>
+          <ContactForm />
+
+          <h2>Contacts</h2>
+          {/* <Filter ... /> */}
+          {/* <ContactList ... /> */}
+        </div>
+        {/* <Input /> */}
+      </div>
+    );
+  }
+}
